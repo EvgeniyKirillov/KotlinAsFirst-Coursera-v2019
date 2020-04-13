030b954ab39d373399152eb254f748442150c050
EvgeniyKirillov <ek@52r.org>
2020-04-13 20:59:50 +0300

    Lesson 1: Angle In radian Third digit
    Lesson 2 task1:
    Age description
    Segment length
    
    Lesson 2 task2:
    Brick passes
    Number Happy

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,6 +3,7 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import kotlin.math.sqrt
 
 /**
  * Пример
@@ -18,7 +19,10 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean =
+    number / 1000 + (number / 100 - number / 1000 * 10) ==
+            (number / 10 - number / 100 * 10) + (number - number / 10 * 10)
+
 
 /**
  * Простая
@@ -59,4 +63,9 @@ fun circleInside(
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean =
+    a <= r && b >= s || b <= r && a >= s || b <= r && c >= s || c <= r && b >= s
+
+
+
+
